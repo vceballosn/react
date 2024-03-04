a687420f0407a294ab8f1587b5cc0b6e7a3adf2a
vladimir.ceballos@gmail.com <vladimir.ceballos@gmail.com>
2024-03-04 18:11:50 -0400

    first practice jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ export function App (){
+
+    return(
+        <article  style={{display:'flex'}}>
+            <header>
+              <img alt="el avatar vladimir jajajas" src="https://unavatar.io/duckduckgo/gummibeer.dev"/>
+              <div>
+                <strong> vladimir Ceballos </strong>
+                <span>@vladimir1978</span>
+              </div>
+            </header>
+            <aside>
+                <button> Seguir</button>
+            </aside>
+        </article>
+    )
+}
