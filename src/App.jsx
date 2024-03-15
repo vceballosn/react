45e4278382dcbfb3b6c98e7fc59a256c24a1f153
vladimir.ceballos@gmail.com <vladimir.ceballos@gmail.com>
2024-03-15 10:20:20 -0400

    tercer commit uso hooks estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
  import './App.css'
 import { TwitterFollowCard } from './TwiterFollowCard'
+import { useState } from 'react'
  
  export function App (){
 
    const formatUserName =(userName) => `@${userName}`
-
+   const vladimir = { isFollowing:true,userName:'vceballos'}
+   const [name,setName] = useState('vladimir Ceballos')
+   console.log('render with name ',name)
     return(
       <div className='App'>
-         <TwitterFollowCard formatUserName={formatUserName} userName="vceballos" name="vladimir ceballos" avatar={"https://unavatar.io/duckduckgo/gummibeer.dev"} isFollowing/>
-         <TwitterFollowCard formatUserName={formatUserName} userName="vceballos" name="vladimir ceballos" avatar={"https://unavatar.io/duckduckgo/gummibeer.dev"} isFollowing={false}/>
-         <TwitterFollowCard formatUserName={formatUserName}  userName="vceballos" name="vladimir ceballos" avatar={"https://unavatar.io/youtube/casey"} isFollowing/>
+         <TwitterFollowCard {...vladimir } formatUserName={formatUserName}   avatar={"https://unavatar.io/duckduckgo/gummibeer.dev"} beginIsFollowing={false}>
+           <strong>vladimir Ilich Ceballos Narvaez Exitos</strong> 
+        </TwitterFollowCard>
+         <TwitterFollowCard formatUserName={formatUserName} userName={name}  avatar={"https://unavatar.io/duckduckgo/gummibeer.dev"} beginIsFollowing={false}>
+          <strong>Mijail Alaberto Ceballos Narvaez </strong>  
+        </TwitterFollowCard>
+         <TwitterFollowCard formatUserName={formatUserName}  userName={name}  avatar={"https://unavatar.io/youtube/casey"} beginIsFollowing>
+          <strong>Vladimir Samael Ceballos Perez</strong> 
+        </TwitterFollowCard>
+        <TwitterFollowCard formatUserName={formatUserName} userName={name}  avatar={"https://unavatar.io/youtube/casey"} beginIsFollowing>
+           {/* REalizado por vladimir Ceballos*/}
+        </TwitterFollowCard>
+
+
+        
+        <button onClick={() => setName('ilich xxxxx')}>
+          Cambio nombre 
+        </button>
       </div>
        
     )
--- src/TwiterFollowCard.jsx
+++ src/TwiterFollowCard.jsx
@@ -1,17 +1,31 @@
-export function TwitterFollowCard({formatUserName,userName,name,isFollowing, avatar}){
-console.log(isFollowing)
+import { useState } from 'react'
+
+
+  export function TwitterFollowCard({children, formatUserName,userName =' User not found', avatar,beginIsFollowing}){
+ 
+  const [isFollowing, setIsFollowing] = useState(beginIsFollowing)
+  const textoButton = isFollowing ? 'Siguiendo':'Seguir'
+  
+  const handleClick = () => {
+    console.log(isFollowing)
+    setIsFollowing(!isFollowing)
+
+  }
+
+
+const clasButon = isFollowing ? 'tw-followCard-button.is-following ':'tw-followCard-button'
      return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
               <img className='tw-followCard-avatar'
                     alt="el avatar vladimir jajajas" src={avatar}/>
               <div  className='tw-followCard-infoUserName'>
-                <strong > {name}</strong>
+                {children}
                 <span>{formatUserName(userName)}</span>
               </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'> Seguir</button>
+                <button className={clasButon} onClick={handleClick}> {textoButton}</button>
             </aside>
         </article>
     )
