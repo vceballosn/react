6fc6d898a6e5d4ba5c642def4afa36f80a36d014
vladimir.ceballos@gmail.com <vladimir.ceballos@gmail.com>
2024-05-07 10:55:18 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,154 @@
- import './App.css'
-import { TwitterFollowCard } from './TwiterFollowCard'
 import { useState } from 'react'
+import './index.css'
 
 
-const users = [{
-  userName:'vcebllos',
-  name:'Vladimir Ilich Ceballos Narvaez',
-  isFollowing:true
-},
 
-{
-  userName:'vperez',
-  name:'Vladimir Samael Ceballos Perez',
-  isFollowing:true
-},
-{
-  userName:'mceballos',
-  name:'Mijail Alberto Ceballos Narvaez',
-  isFollowing:true
+
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
 }
+
+
+const WINNER_COMBOS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8],
+  
 ]
- 
- export function App (){
 
-   
-   return(   
-        <section className='App'>
-          {
-          users.map (user => {
-            const {userName,name,isFollowing} =  user
-            return(
-              <TwitterFollowCard key={userName}userName={userName}name={name}isFollowing={isFollowing}  avatar={"https://unavatar.io/duckduckgo/gummibeer.dev"} >
-                {name}
-              </TwitterFollowCard>
-            )
-          })   
-        }   
-      </section>
-       )
+function App() {
+
+  const TURNS = {
+    X: 'X',
+    O: '0'
+  }
+
+
+  const [board, setBoard] = useState(Array(9).fill(null)) // null es que no hay ganador  false es que hay empate 
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+
+  const checkWinner = (boardToCheck)=>{
+    for (const combo of WINNER_COMBOS) {
+     const [a,b,c] = combo
+        if( boardToCheck[a] && // 0 =>  X or O contenga 
+
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+        ){
+          return boardToCheck[a] //0 =>  X or O contenga 
+        }
+
+    }
+    return null
+
+  }
+
+  const resetGame =() =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+
+    // no actualizamos esta posición
+    // si ya tiene algo
+  
+    if (board[index]  || winner) return
+    // actualizar el tablero 
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+
+    setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){  // estados asingronos
+      setWinner(newWinner)
+    }
+
+  }
+
+  return (
+    <main>
+      <h1 className="board">
+        The Game Old Make For Vladimir Ceballos
+        <button onClick={resetGame}> Reset del Juego </button>
+      </h1>
     
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} updateBoard={updateBoard} index={index}>
+                {board[index]}
+              </Square>
+
+            )
+          })
+        }
+
+      </section>
+
+
+
+      {TURNS.X == TURNS.X}
+
+
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+      {
+      winner != null && (
+        <section className="winner">
+          
+          <div className="text">
+            <h2>
+              {
+                winner === false
+                ? 'Empate':'Gano '
+              }
+            </h2>  
+            <header className="win">
+              {winner && <Square>{winner} </Square>}
+            </header>
+
+            <footer>
+             <button onClick={resetGame}>
+                 Empezar de nuevo 
+              </button>  
+            </footer>
+          </div>
+          
+        </section>
+      )
+    }
+    </main>
+  )
 }
+
+export default App
